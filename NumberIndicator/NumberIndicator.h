cd1c4afb8b3044189f152fcf5a781fef5b1f9783
musicode <musicode@qq.com>
2018-12-14 17:58:40 +0800

    Initial Commit

---
+++ NumberIndicator/NumberIndicator.h
@@ -0,0 +1,19 @@
+//
+//  NumberIndicator.h
+//  NumberIndicator
+//
+//  Created by zhujl on 2018/12/14.
+//  Copyright © 2018年 finstao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NumberIndicator.
+FOUNDATION_EXPORT double NumberIndicatorVersionNumber;
+
+//! Project version string for NumberIndicator.
+FOUNDATION_EXPORT const unsigned char NumberIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NumberIndicator/PublicHeader.h>
+
+
