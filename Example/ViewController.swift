5eedc9925be50a59075a171f16f2980d1ff3cb88
musicode <musicode@qq.com>
2018-12-14 19:07:24 +0800

    update

---
+++ Example/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by zhujl on 2018/12/14.
+//  Copyright © 2018年 finstao. All rights reserved.
+//
+
+import UIKit
+import NumberIndicator
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let indicator = NumberIndicator()
+        indicator.index = 2
+        indicator.count = 10
+        indicator.separator = "/"
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(indicator)
+        view.backgroundColor = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
+        
+        view.addConstraints([
+            NSLayoutConstraint(item: indicator, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: indicator, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0)
+        ])
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
